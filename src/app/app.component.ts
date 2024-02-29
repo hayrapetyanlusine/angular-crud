f5e7eb068f1a29d42a98d5f2ba65627eea46fbae
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2024-02-29 19:55:12 +0400

    add create form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormFieldModule, MatInputModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,34 +1,47 @@
-import {Component} from '@angular/core';
-import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+import {NgIf} from "@angular/common";
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-create-user',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf,
+    MatInputModule,
+    MatFormFieldModule
   ],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
-export class CreateUserComponent {
-  createUserForm = this.formBuilder.group({
-    name: ["", Validators.required],
-    email: ["", Validators.required],
-    company: this.formBuilder.group({
-      name: [""]
-    })
-  });
+export class CreateUserComponent implements OnInit {
+  createUserForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router
   ) {}
 
-  resetValues() {
+  ngOnInit(): void {
+    this.createUserForm = this.formBuilder.group({
+      name: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/^[a-zA-Z]*$/)]
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      company: this.formBuilder.group({
+        name: ['']
+      })
+    });
+  }
+
+  resetValues(): void {
     this.createUserForm.patchValue({
-      name: "",
-      email: "",
+      name: "", email: "",
       company: {
         name: ""
       }
@@ -38,6 +51,30 @@ export class CreateUserComponent {
   onSubmit(): void {
     const formData = this.createUserForm.value;
 
-    this.router.navigate([""], { state: { data: formData } });
+    this.router.navigate([""], {state: {data: formData}});
+  }
+
+  getNameErrorMessage() {
+    const nameFormControl = this.createUserForm.get('name');
+
+    if (nameFormControl?.hasError("required")) {
+      return "Name is required";
+    }
+
+    if (nameFormControl?.hasError("minlength")) {
+      return "Name must be at least 3 characters";
+    }
+
+    return nameFormControl?.hasError("pattern") ? "Only string" : "";
+  }
+
+  getEmailErrorMessage() {
+    const emailFormControl = this.createUserForm.get("email");
+
+    if (emailFormControl?.hasError("required")) {
+      return "Email is required";
+    }
+
+    return emailFormControl?.hasError("email") ? "Not a valid email" : "";
   }
 }
